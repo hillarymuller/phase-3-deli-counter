8123beafafd69a8b2b832d8ab4d57c86859df09c
Hillary Muller <hillaryaward@gmail.com>
2022-08-12 16:52:28 -0500

    commit tree a092f140438fb1cef6741275ec819f6581cc4f80

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,1 +1,20 @@
-# Write your code here.
+
+def line(people_in_line)
+    if people_in_line.empty?
+       puts "The line is currently empty."
+    else
+        people = people_in_line.map.with_index(1) do |name, index|
+            "#{index}. #{name}"
+        end
+        puts "The line is currently: " + people.join(" ")
+    end
+end
+
+def take_a_number(people_in_line, name)
+    people_in_line << name
+    puts "Welcome, #{name}. You are number #{people_in_line.length} in line."
+end
+
+def now_serving(people_in_line)
+    puts people_in_line.empty? ? "There is nobody waiting to be served!" : "Currently serving #{people_in_line.shift}."
+end
